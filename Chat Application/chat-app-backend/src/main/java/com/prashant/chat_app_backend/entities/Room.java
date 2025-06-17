518184d0f75fa26f91836e4e0f1d88b65491b443
Prasant Singh <prasant2@amityonline.com>
2025-06-17 14:56:00 +0530

    commit chat backend

--- Chat Application/chat-app-backend/src/main/java/com/prashant/chat_app_backend/entities/Room.java
+++ Chat Application/chat-app-backend/src/main/java/com/prashant/chat_app_backend/entities/Room.java
@@ -1,19 +1,28 @@
 package com.prashant.chat_app_backend.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
+@Document(collection = "rooms")
 @Getter
-@Entity
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Room {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private String roomId;
   private String name;
+  private List<Message> messages = new ArrayList<>();
 
 }

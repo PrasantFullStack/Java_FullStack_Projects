3afd97696fdbfa6136ef86bf82dc2d97a30a6a8f
Prasant Singh <prasant2@amityonline.com>
2025-06-19 20:43:54 +0530

    feat: Add global exception handling and resource not found exception classes

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/MainService.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/MainService.java
@@ -1,29 +1,31 @@
-package com.prashant.apna.bazar.services;
+// package com.prashant.apna.bazar.services;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.multipart.MultipartFile;
 
-import com.prashant.apna.bazar.models.MaincategoryDto;
-import com.prashant.apna.bazar.repositories.MainRepo;
-import com.prashant.apna.bazar.responseDto.MainResponseDto;
-import com.prashant.apna.bazar.utils.FileUploadUtil;
+// import com.prashant.apna.bazar.models.MaincategoryDto;
+// import com.prashant.apna.bazar.repositories.MainRepo;
+// import com.prashant.apna.bazar.responseDto.MainResponseDto;
+// import com.prashant.apna.bazar.utils.FileUploadUtil;
 
-@Service
-public class MainService {
-  @Autowired
-  private MainRepo mainRepo;
+// @Service
+// public class MainService {
+// @Autowired
+// private MainRepo mainRepo;
 
-  // File upload directory
-  private static final String uploadDir = FileUploadUtil.getUploadDirFor("maincategories");
+// // File upload directory
+// private static final String uploadDir =
+// FileUploadUtil.getUploadDirFor("maincategories");
 
-  // create maincategory
+// // // create maincategory
 
-  public MainResponseDto createMaincategory(MaincategoryDto mainDto, MultipartFile file)throws IOException{
- 
-return 
-  }
+// // public MainResponseDto createMaincategory(MaincategoryDto mainDto,
+// // MultipartFile file)throws IOException{
 
-}
+// // return
+// // }
+
+// }

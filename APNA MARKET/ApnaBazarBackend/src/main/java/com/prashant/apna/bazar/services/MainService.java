b69cf3ac80079d0159329d9aefc52b685f3db695
Prasant Singh <prasant2@amityonline.com>
2025-06-19 12:44:46 +0530

    feat: Add createMaincategory method and file upload directory in MainService

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/MainService.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/MainService.java
@@ -1,15 +1,29 @@
 package com.prashant.apna.bazar.services;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.prashant.apna.bazar.models.MaincategoryDto;
 import com.prashant.apna.bazar.repositories.MainRepo;
+import com.prashant.apna.bazar.responseDto.MainResponseDto;
+import com.prashant.apna.bazar.utils.FileUploadUtil;
 
 @Service
 public class MainService {
   @Autowired
   private MainRepo mainRepo;
 
+  // File upload directory
+  private static final String uploadDir = FileUploadUtil.getUploadDirFor("maincategories");
+
   // create maincategory
+
+  public MainResponseDto createMaincategory(MaincategoryDto mainDto, MultipartFile file)throws IOException{
+ 
+return 
+  }
 
 }

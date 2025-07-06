20141b63edf4f18bd34944425a277c11429d78f9
Prasant Singh <prasant2@amityonline.com>
2025-07-07 01:22:47 +0530

    feat: Implement multipart form data handling for brand creation and updates

--- APNA MARKET/Frontend/ecom/src/Pages/Admin/Brand/AdminCreateBrand.jsx
+++ APNA MARKET/Frontend/ecom/src/Pages/Admin/Brand/AdminCreateBrand.jsx
@@ -1,122 +1,172 @@
-import React, { useEffect, useState } from 'react'
-import Breadcrum from '../../../Components/Breadcrum'
-import Sidebar from '../Sidebar'
-import { Link, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import Breadcrum from "../../../Components/Breadcrum";
+import Sidebar from "../Sidebar";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import FormValidator from '../../../Validators/FormValidator'
-import ImageValidator from '../../../Validators/ImageValidator'
+import FormValidator from "../../../Validators/FormValidator";
+import ImageValidator from "../../../Validators/ImageValidator";
 
-import { getBrand, createBrand } from "../../../Redux/ActionCreators/BrandActionCreators"
+import {
+  getBrand,
+  createMultipartRecord,
+} from "../../../Redux/ActionCreators/BrandActionCreators";
+import { toast } from "react-toastify";
 export default function AdminCreateBrand() {
-    let [data, setData] = useState({
-        name: "",
-        pic: "",
-        active: true
-    })
+  let [data, setData] = useState({
+    name: "",
+    pic: "",
+    active: true,
+  });
 
-    let [errorMessage, setErrorMessage] = useState({
-        name: "Name Field Is Mendatory",
-        pic: "Pic Field Is Mendatory"
-    })
+  let [errorMessage, setErrorMessage] = useState({
+    name: "Name Field Is Mendatory",
+    pic: "Pic Field Is Mendatory",
+  });
 
-    let [show, setShow] = useState(false)
-    let navigate = useNavigate()
+  let [show, setShow] = useState(false);
+  let navigate = useNavigate();
 
-    let BrandStateData = useSelector(state => state.BrandStateData)
-    let dispatch = useDispatch()
+  let BrandStateData = useSelector((state) => state.BrandStateData);
+  let dispatch = useDispatch();
 
+  function getInputData(e) {
+    var name = e.target.name;
+    // var value = e.target.files && e.target.files.length ? "brand/" + e.target.files[0].name : e.target.value
+    var value =
+      e.target.files && e.target.files.length
+        ? e.target.files[0]
+        : e.target.value;
 
-    function getInputData(e) {
-        var name = e.target.name
-        var value = e.target.files && e.target.files.length ? "brand/" + e.target.files[0].name : e.target.value
-        // var value = e.target.files && e.target.files.length ? e.target.files[0] : e.target.value
+    setErrorMessage((old) => {
+      toast.warn("Field Validation Error");
+      return {
+        ...old,
+        [name]: e.target.files ? ImageValidator(e) : FormValidator(e),
+      };
+    });
 
+    setData((old) => {
+      return {
+        ...old,
+        [name]: name === "active" ? (value === "1" ? true : false) : value,
+      };
+    });
+  }
+
+  function postData(e) {
+    e.preventDefault();
+    let error = Object.values(errorMessage).find((x) => x !== "");
+    if (error) setShow(true);
+    else {
+      let item = BrandStateData.find(
+        (x) => x.name.toLowerCase() === data.name.toLowerCase()
+      );
+      if (item) {
+        setShow(true);
+        toast.warn("Brand With Same Name Already Exist");
         setErrorMessage((old) => {
-            return {
-                ...old,
-                [name]: e.target.files ? ImageValidator(e) : FormValidator(e)
-            }
-        })
+          return {
+            ...old,
+            name: "Brand With Same Name Already Exist",
+          };
+        });
+        return;
+      }
+      //Create formData object
+      let formData = new FormData();
+      formData.append(
+        "data",
+        JSON.stringify({ name: data.name, active: data.active })
+      );
 
-        setData((old) => {
-            return {
-                ...old,
-                [name]: name === "active" ? (value === "1" ? true : false) : value
-            }
-        })
+      if (data.pic && data.pic instanceof File) {
+        formData.append("pic", data.pic);
+      }
+      //Dispatch action to create brand
+      dispatch(createMultipartRecord(formData));
+      // Reset form data
+      toast.success("Brand Created Successfully");
+      navigate("/admin/brand");
     }
+  }
 
-    function postData(e) {
-        e.preventDefault()
-        let error = Object.values(errorMessage).find(x => x !== "")
-        if (error)
-            setShow(true)
-        else {
-            let item = BrandStateData.find(x => x.name.toLowerCase() === data.name.toLowerCase())
-            if (item) {
-                setShow(true)
-                setErrorMessage((old) => {
-                    return {
-                        ...old,
-                        'name': 'Brand With Same Name Already Exist'
-                    }
-                })
-                return
-            }
-            dispatch(createBrand({ ...data }))
-            // let formData = new FormData()
-            // formData.append("name", data.name)
-            // formData.append("pic", data.pic)
-            // formData.append("active", data.active)
-            // dispatch(createBrand(formData))
-            navigate("/admin/brand")
-        }
-    }
+  useEffect(() => {
+    dispatch(getBrand());
+  }, [BrandStateData.length]);
+  return (
+    <>
+      <Breadcrum title="Admin" />
+      <div className="container-fluid my-3">
+        <div className="row">
+          <div className="col-md-3">
+            <Sidebar />
+          </div>
+          <div className="col-md-9">
+            <h5 className="bg-primary w-100 p-2 text-light text-center">
+              Brand{" "}
+              <Link to="/admin/brand">
+                <i className="fa fa-long-arrow-left text-light float-end"></i>
+              </Link>
+            </h5>
+            <form onSubmit={postData}>
+              <div className="mb-3">
+                <label>Name*</label>
+                <input
+                  type="text"
+                  name="name"
+                  onChange={getInputData}
+                  placeholder="Brand Name"
+                  className={`form-control border-3 ${
+                    show && errorMessage.name
+                      ? "border-danger"
+                      : "border-primary"
+                  }`}
+                />
+                {show && errorMessage.name ? (
+                  <p className="text-danger">{errorMessage.name}</p>
+                ) : null}
+              </div>
 
-
-    useEffect(() => {
-        dispatch(getBrand())
-    }, [BrandStateData.length])
-    return (
-        <>
-            <Breadcrum title="Admin" />
-            <div className="container-fluid my-3">
-                <div className="row">
-                    <div className="col-md-3">
-                        <Sidebar />
-                    </div>
-                    <div className="col-md-9">
-                        <h5 className='bg-primary w-100 p-2 text-light text-center'>Brand <Link to="/admin/brand"><i className='fa fa-long-arrow-left text-light float-end'></i></Link></h5>
-                        <form onSubmit={postData}>
-                            <div className="mb-3">
-                                <label>Name*</label>
-                                <input type="text" name="name" onChange={getInputData} placeholder='Brand Name' className={`form-control border-3 ${show && errorMessage.name ? 'border-danger' : 'border-primary'}`} />
-                                {show && errorMessage.name ? <p className='text-danger'>{errorMessage.name}</p> : null}
-                            </div>
-
-                            <div className="row">
-                                <div className="col-md-6 mb-3">
-                                    <label>Pic*</label>
-                                    <input type="file" name="pic" onChange={getInputData} className={`form-control border-3 ${show && errorMessage.pic ? 'border-danger' : 'border-primary'}`} />
-                                    {show && errorMessage.pic ? <p className='text-danger'>{errorMessage.pic}</p> : null}
-                                </div>
-                                <div className="col-md-6 mb-3">
-                                    <label>Active*</label>
-                                    <select name="active" onChange={getInputData} className='form-select border-3 border-primary'>
-                                        <option value="1">Yes</option>
-                                        <option value="0">NO</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div className="mb-3">
-                                <button type="submit" className='btn btn-primary w-100'>Create</button>
-                            </div>
-                        </form>
-                    </div>
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <label>Pic*</label>
+                  <input
+                    type="file"
+                    name="pic"
+                    onChange={getInputData}
+                    className={`form-control border-3 ${
+                      show && errorMessage.pic
+                        ? "border-danger"
+                        : "border-primary"
+                    }`}
+                  />
+                  {show && errorMessage.pic ? (
+                    <p className="text-danger">{errorMessage.pic}</p>
+                  ) : null}
                 </div>
-            </div>
-        </>
-    )
+                <div className="col-md-6 mb-3">
+                  <label>Active*</label>
+                  <select
+                    name="active"
+                    onChange={getInputData}
+                    className="form-select border-3 border-primary"
+                  >
+                    <option value="1">Yes</option>
+                    <option value="0">NO</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <button type="submit" className="btn btn-primary w-100">
+                  Create
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }

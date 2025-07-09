1a0c725f58d60e53b0932dced0642e9e0c4d6300
Prasant Singh <prasant2@amityonline.com>
2025-07-09 11:31:20 +0530

    feat: Implement multipart form handling for testimonials and update sagas

--- APNA MARKET/Frontend/ecom/src/Redux/ActionCreators/TestimonialActionCreators.jsx
+++ APNA MARKET/Frontend/ecom/src/Redux/ActionCreators/TestimonialActionCreators.jsx
@@ -1,28 +1,40 @@
-import { CREATE_TESTIMONIAL, DELETE_TESTIMONIAL, GET_TESTIMONIAL, UPDATE_TESTIMONIAL } from "../Constants"
+import {
+  CREATE_TESTIMONIAL,
+  DELETE_TESTIMONIAL,
+  GET_TESTIMONIAL,
+  UPDATE_TESTIMONIAL,
+} from "../Constants";
 
-export function createTestimonial(data) {
-    return {
-        type: CREATE_TESTIMONIAL,
-        payload: data
-    }
+// export function createTestimonial(data) {
+//     return {
+//         type: CREATE_TESTIMONIAL,
+//         payload: data
+//     }
+// }
+
+export function createMultpartForm(formData) {
+  return {
+    type: CREATE_TESTIMONIAL,
+    payload: formData,
+  };
 }
 
 export function getTestimonial() {
-    return {
-        type: GET_TESTIMONIAL
-    }
+  return {
+    type: GET_TESTIMONIAL,
+  };
 }
 
-export function updateTestimonial(data) {
-    return {
-        type: UPDATE_TESTIMONIAL,
-        payload: data
-    }
+export function updateMultipartRecord(data) {
+  return {
+    type: UPDATE_TESTIMONIAL,
+    payload: data,
+  };
 }
 
 export function deleteTestimonial(data) {
-    return {
-        type: DELETE_TESTIMONIAL,
-        payload: data
-    }
+  return {
+    type: DELETE_TESTIMONIAL,
+    payload: data,
+  };
 }

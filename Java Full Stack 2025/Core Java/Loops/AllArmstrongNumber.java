0b957ee3ee7c29b1418efaf99588f3f4a2ffa786
Prasant Singh <prasant2@amityonline.com>
2025-07-11 12:53:38 +0530

    program PerfectNumber using loop and if- else condition

---
+++ Java Full Stack 2025/Core Java/Loops/AllArmstrongNumber.java
@@ -0,0 +1,19 @@
+package Loops;
+
+public class AllArmstrongNumber {
+  // Program: Print All ArmstrongNumber from 1 to 1000;
+  public static void main(String[] args) {
+    for (int number = 1; number <= 1000; number++) {
+      int temp = number, sum = 0;
+      while (temp != 0) {
+        int digit = temp % 10;
+        sum += digit * digit;
+        temp /= 10;
+      }
+      if (sum == number) {
+        System.out.println(number + " ");
+
+      }
+    }
+  }
+}
---
+++ Java Full Stack 2025/Core Java/Loops/PerfectNumber.java
@@ -0,0 +1,23 @@
+package Loops;
+
+//Perfect Number: Sum of divisors (excluding it self) = number
+//Example: 28 -> 1+2+4+7+14= 28
+public class PerfectNumber {
+  public static void main(String[] args) {
+    int num = 28, sum = 0;
+
+    for (int i = 1; i < num; i++) {
+      if (num % i == 0) {
+        sum += i;
+
+      }
+      if (sum == num) {
+        System.out.println("Perfect Number :" + " " + sum);
+
+      } else {
+        System.out.println("Not perfect Number:" + " " + sum);
+
+      }
+    }
+  }
+}

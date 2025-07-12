9e3c07a122383de68120c9fb1039eb0000b9deac
Prasant Singh <prasant2@amityonline.com>
2025-07-12 17:53:11 +0530

    test fibonacciSeries

---
+++ Java Full Stack 2025/Core Java/Loops/Test.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class Test {
+  public static void main(String[] args) {
+    int num = 15;
+    int a = 0, b = 1;
+
+    for (int i = 2; i <= num; i++) {
+      int c = a + b;
+      System.out.println(c + " ");
+      a = b;
+      b = c;
+
+    }
+
+  }
+}

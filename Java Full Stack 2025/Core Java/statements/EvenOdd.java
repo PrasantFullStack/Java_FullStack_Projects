1cf1f5ba2690195c9387e19baf4ccdc1bb09d8ea
Prasant Singh <prasant2@amityonline.com>
2025-07-15 01:14:28 +0530

    EvenOdd program

--- Java Full Stack 2025/Core Java/statements/EvenOdd.java
+++ Java Full Stack 2025/Core Java/statements/EvenOdd.java
@@ -1,16 +1,26 @@
 package statements;
 
+import java.util.Scanner;
+
 public class EvenOdd {
   public static void main(String[] args) {
-    int myNumber = 7;
+    // int myNumber = 7;
 
-    if (myNumber % 2 == 0) {
-      System.out.println("My Number is Even:" + " " + myNumber);
+    // if (myNumber % 2 == 0) {
+    // System.out.println("My Number is Even:" + " " + myNumber);
 
+    // } else {
+    // System.out.println("My number is Odd:" + " " + myNumber);
+    // }
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num = sc.nextInt();
+
+    if (num % 2 == 0) {
+      System.out.println(num + " is Even");
     } else {
-      System.out.println("My number is Odd:" + " " + myNumber);
+      System.out.println(num + " is Odd");
     }
-
   }
 
 }

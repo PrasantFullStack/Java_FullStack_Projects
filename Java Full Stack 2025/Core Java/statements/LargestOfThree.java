097a8cd27b07b2f51649e54c92c807091be3bdc6
Prasant Singh <prasant2@amityonline.com>
2025-07-15 01:27:11 +0530

    find LargestOfthree element

---
+++ Java Full Stack 2025/Core Java/statements/LargestOfThree.java
@@ -0,0 +1,18 @@
+package statements;
+
+import java.util.Scanner;
+
+public class LargestOfThree {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter 3 numbers: ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+
+    int max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+    System.out.println("Largest number is: " + max);
+  }
+}

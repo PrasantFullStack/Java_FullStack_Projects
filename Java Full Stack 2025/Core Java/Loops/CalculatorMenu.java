957968e77cbf11c7757556ef45a6dcae54990bb8
Prasant Singh <prasant2@amityonline.com>
2025-07-17 21:04:35 +0530

    commit

---
+++ Java Full Stack 2025/Core Java/Loops/CalculatorMenu.java
@@ -0,0 +1,44 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class CalculatorMenu {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int choice;
+    do {
+      System.out.println("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
+      System.out.print("Enter your choice: ");
+      choice = sc.nextInt();
+
+      if (choice >= 1 && choice <= 4) {
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        switch (choice) {
+          case 1:
+            System.out.println("Sum: " + (a + b));
+            break;
+          case 2:
+            System.out.println("Difference: " + (a - b));
+            break;
+          case 3:
+            System.out.println("Product: " + (a * b));
+            break;
+          case 4:
+            if (b != 0)
+              System.out.println("Quotient: " + (a / b));
+            else
+              System.out.println("Cannot divide by zero!");
+            break;
+        }
+      } else if (choice != 5) {
+        System.out.println("Invalid choice.");
+      }
+    } while (choice != 5);
+
+    System.out.println("Calculator closed.");
+  }
+
+}
---
+++ Java Full Stack 2025/Core Java/Loops/Palindrome.java
@@ -0,0 +1,22 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int original = num, reverse = 0;
+
+        while (num != 0) {
+            reverse = reverse * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (original == reverse)
+            System.out.println("Palindrome Number");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}

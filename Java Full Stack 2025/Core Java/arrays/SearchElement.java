2df81105f07b6dfd2d7cef2c25fc00bdf2dcdf0e
Prasant Singh <prasant2@amityonline.com>
2025-07-18 21:47:44 +0530

    SearchElement

---
+++ Java Full Stack 2025/Core Java/arrays/SearchElement.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class SearchElement {
+  public static void main(String[] args) {
+    int[] arr = { 11, 22, 33, 44, 55 };
+    int key = 3;
+    boolean found = false;
+
+    for (int num : arr) {
+      if (num == key) {
+        found = true;
+        break;
+      }
+    }
+    System.out.println(found ? "Found" : "Not Found");
+  }
+}

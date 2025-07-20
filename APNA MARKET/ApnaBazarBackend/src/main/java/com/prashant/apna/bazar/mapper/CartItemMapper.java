d483c2278753708033847facc9aebc4849072c8f
Prasant Singh <prasant2@amityonline.com>
2025-07-20 23:57:33 +0530

    dto to entity

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/CartItemMapper.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/CartItemMapper.java
@@ -1,5 +1,9 @@
 package com.prashant.apna.bazar.mapper;
 
+import com.prashant.apna.bazar.entities.CartItem;
+
 public class CartItemMapper {
+  // dto to entity
+public CartItem toEntityCartItem(CartDto)
 
 }

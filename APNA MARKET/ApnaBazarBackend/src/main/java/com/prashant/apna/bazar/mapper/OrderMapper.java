e194f32d91b660d70564c98e0a6f05ca16484afe
Prasant Singh <prasant2@amityonline.com>
2025-07-21 00:02:41 +0530

    add Order Mapper dto to entity

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/OrderMapper.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/OrderMapper.java
@@ -1,5 +1,16 @@
 package com.prashant.apna.bazar.mapper;
 
+import org.springframework.beans.BeanUtils;
+
+import com.prashant.apna.bazar.entities.Order;
+import com.prashant.apna.bazar.payload.request.OrderDto;
+
 public class OrderMapper {
+  // Dto to Entity
+  public Order toEntityOrder(OrderDto orderDto) {
+    Order order = new Order();
+    BeanUtils.copyProperties(orderDto, order);
+    return order;
+  }
 
 }

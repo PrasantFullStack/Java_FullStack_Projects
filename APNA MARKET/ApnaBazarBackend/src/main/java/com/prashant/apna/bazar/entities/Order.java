718dde90992843ad5f8dca8257585619f38c8a60
Prasant Singh <prasant2@amityonline.com>
2025-07-20 22:36:45 +0530

    add fields Order Entity

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/entities/Order.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/entities/Order.java
@@ -1,5 +1,28 @@
 package com.prashant.apna.bazar.entities;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Order {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private Long user;
+  private String orderStatus;
+  private String paymentMode;
+  private String paymentStatus;
+  private double subtotal;
+  private double total;
+  private LocalDateTime data;
+
+  @OneToMany
+  private List<CartItem> products;
 
 }

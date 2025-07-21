275e993cee589bb0a0e6ac1706fea3284cfd00d7
Prasant Singh <prasant2@amityonline.com>
2025-07-21 23:05:41 +0530

    In cartItemMapper add product find by id and set cartItem

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/CartItemMapper.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/CartItemMapper.java
@@ -1,15 +1,26 @@
 package com.prashant.apna.bazar.mapper;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.prashant.apna.bazar.entities.CartItem;
+import com.prashant.apna.bazar.entities.Product;
 import com.prashant.apna.bazar.payload.request.CartItemDto;
 import com.prashant.apna.bazar.payload.response.CartItemResponse;
+import com.prashant.apna.bazar.repositories.ProductRepo;
 
 public class CartItemMapper {
+
+  @Autowired
+  private ProductRepo productRepo;
+
   // dto to entity
   public CartItem toEntityCartItem(CartItemDto cartItemDto) {
     CartItem entityCartItem = new CartItem();
+    // Set Product
+    Product product = productRepo.findById(cartItemDto.getProductId())
+        .orElseThrow(() -> new RuntimeException("Product Not Found"));
+    entityCartItem.setProduct(product);
     BeanUtils.copyProperties(cartItemDto, entityCartItem);
     return entityCartItem;
   }

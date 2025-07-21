cf13c6f3253fab0ca34b01193d6bf320cf3d258d
Prasant Singh <prasant2@amityonline.com>
2025-07-21 23:08:54 +0530

    create  setter getter  allagrsConstructor and NoArgsConstructor

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/entities/CartItem.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/entities/CartItem.java
@@ -5,7 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Entity
 public class CartItem {
   @Id

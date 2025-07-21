2af69fe62d94e80cb1e3e123c7ce9221953b20f5
Prasant Singh <prasant2@amityonline.com>
2025-07-21 14:25:06 +0530

    save order

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/CheckoutService.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/CheckoutService.java
@@ -1,5 +1,37 @@
 package com.prashant.apna.bazar.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.prashant.apna.bazar.entities.Order;
+import com.prashant.apna.bazar.mapper.OrderMapper;
+import com.prashant.apna.bazar.payload.request.OrderDto;
+import com.prashant.apna.bazar.payload.response.OrderResponse;
+import com.prashant.apna.bazar.repositories.CartItemRepo;
+import com.prashant.apna.bazar.repositories.OrderRepo;
+import com.prashant.apna.bazar.repositories.ProductRepo;
+
+@Service
 public class CheckoutService {
+
+  @Autowired
+  private OrderRepo orderRepo;
+
+  @Autowired
+  private ProductRepo productRepo;
+
+  @Autowired
+  private CartItemRepo cartItemRepo;
+
+  @Autowired
+  private OrderMapper mapper;
+
+  // save the order to cart
+
+  public OrderResponse placeOrder(OrderDto orderDto) {
+    // save order
+    Order order = m;
+
+  }
 
 }

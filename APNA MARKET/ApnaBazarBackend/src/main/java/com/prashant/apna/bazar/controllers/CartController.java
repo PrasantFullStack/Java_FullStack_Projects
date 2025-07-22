3e70f272efbba1019f00ee6dababd07d398c243a
Prasant Singh <prasant2@amityonline.com>
2025-07-22 22:37:43 +0530

    In CartController add UpdateCartItem

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/CartController.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/CartController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,11 @@ public class CartController {
   @PostMapping
   ResponseEntity<CartItemResponse> addCartItem(@RequestBody CartItemDto cartItemDto) {
     return ResponseEntity.status(HttpStatus.CREATED).body(cartService.addCart(cartItemDto));
+  }
+
+  @PutMapping
+  ResponseEntity<CartItemResponse> updateCartItem(@RequestBody CartItemDto cartItemDto) {
+
   }
 
 }

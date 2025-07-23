6f486c6ab10585dc09b48f91c8d9c16fdc26f959
Prasant Singh <prasant2@amityonline.com>
2025-07-24 01:03:09 +0530

    create logic newsletter by id method in NewsletterService class

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/NewsletterService.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/NewsletterService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.prashant.apna.bazar.entities.Newsletter;
+import com.prashant.apna.bazar.exception.ResourceNotFoundException;
 import com.prashant.apna.bazar.mapper.NewsletterMapper;
 import com.prashant.apna.bazar.payload.request.NewsletterDto;
 import com.prashant.apna.bazar.payload.response.NewsResponseDto;
@@ -31,5 +32,12 @@ public class NewsletterService {
   // GetAll newsletters
   public List<NewsResponseDto> getAllNewsletters() {
     return newsletterRepo.findAll().stream().map(newsletterMapper::toResponse).toList();
+  }
+
+  // Newsletter by id
+  public NewsResponseDto getNewsletter(Long id) {
+    Newsletter savedNewsletter = newsletterRepo.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Newsletter not found by id:" + id));
+    return newsletterMapper.toResponse(savedNewsletter);
   }
 }

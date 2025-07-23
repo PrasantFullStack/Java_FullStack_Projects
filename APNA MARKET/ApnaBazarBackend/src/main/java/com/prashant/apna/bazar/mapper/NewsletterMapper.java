93e28210dc89e45d721a8e1b46dcd803541ec4ab
Prasant Singh <prasant2@amityonline.com>
2025-07-23 23:31:52 +0530

    Create Mapper class for Mapping Dto to entity and map  entity to ResponseDto

---
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/NewsletterMapper.java
@@ -0,0 +1,17 @@
+package com.prashant.apna.bazar.mapper;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import com.prashant.apna.bazar.entities.Newsletter;
+import com.prashant.apna.bazar.payload.request.NewsletterDto;
+
+@Component
+public class NewsletterMapper {
+  // map dto to entity
+  public Newsletter toEntity(NewsletterDto newsletterDto) {
+    Newsletter newsletter = new Newsletter();
+    BeanUtils.copyProperties(newsletterDto, newsletter);
+    return newsletter;
+  }
+}

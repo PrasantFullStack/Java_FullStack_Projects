b3097eeda900709e7d1de40db94156bfccbafca5
Prasant Singh <prasant2@amityonline.com>
2025-07-24 02:48:45 +0530

    add createNewsletter in NewsletterController

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/NewsletterController.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/NewsletterController.java
@@ -1,8 +1,26 @@
 package com.prashant.apna.bazar.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prashant.apna.bazar.payload.request.NewsletterDto;
+import com.prashant.apna.bazar.payload.response.NewsResponseDto;
+import com.prashant.apna.bazar.services.NewsletterService;
+
 @RestController
+@RequestMapping("/newsletter")
 public class NewsletterController {
+
+  @Autowired
+  private NewsletterService newsletterService;
+
+  ResponseEntity<NewsResponseDto> createNewslettter(@RequestBody NewsletterDto newsletterDto) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(newsletterService.createNewsletter(newsletterDto));
+  }
 
 }

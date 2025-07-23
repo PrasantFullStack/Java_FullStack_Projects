e7785811bbe9ce1972c652dfd3eabf8fb31c50e4
Prasant Singh <prasant2@amityonline.com>
2025-07-24 05:23:23 +0530

    add delete Newsletter by id in NewsletterController

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/NewsletterController.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/NewsletterController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prashant.apna.bazar.payload.request.NewsletterDto;
@@ -40,6 +41,12 @@ public class NewsletterController {
   @GetMapping("/{id}")
   ResponseEntity<NewsResponseDto> getNewsletterById(@PathVariable Long id) {
     return ResponseEntity.status(HttpStatus.OK).body(newsletterService.getNewsletter(id));
+  }
+
+  // delete Newsletter by id
+  @DeleteMapping("/{id}")
+  ResponseEntity<NewsResponseDto> deleteNewsletter(@PathVariable Long id) {
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(newsletterService.deleteNewsletter(id));
   }
 
 }

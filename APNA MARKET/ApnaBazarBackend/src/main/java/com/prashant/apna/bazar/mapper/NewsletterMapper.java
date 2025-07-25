6e3845cae68405925604b4b0528adf9e0f1e1b41
Prasant Singh <prasant2@amityonline.com>
2025-07-25 21:51:09 +0530

    In NewsletterMappar improve

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/NewsletterMapper.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/NewsletterMapper.java
@@ -1,6 +1,7 @@
 package com.prashant.apna.bazar.mapper;
 
 import org.springframework.beans.BeanUtils;
+
 import org.springframework.stereotype.Component;
 
 import com.prashant.apna.bazar.entities.Newsletter;
@@ -10,6 +11,7 @@ import com.prashant.apna.bazar.payload.response.NewsResponseDto;
 @Component
 public class NewsletterMapper {
   // map dto to entity
+
   public Newsletter toEntity(NewsletterDto newsletterDto) {
     Newsletter newsletter = new Newsletter();
     BeanUtils.copyProperties(newsletterDto, newsletter);
@@ -17,6 +19,7 @@ public class NewsletterMapper {
   }
 
   // map entity to Response
+
   public NewsResponseDto toResponse(Newsletter savedNewsletter) {
     NewsResponseDto response = new NewsResponseDto();
     BeanUtils.copyProperties(savedNewsletter, response);

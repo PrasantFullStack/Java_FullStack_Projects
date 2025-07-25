28ce813baffda2eb3ad1e291b2aa1eb7e8ad311a
Prasant Singh <prasant2@amityonline.com>
2025-07-26 02:22:06 +0530

    commit init improve code add column annotation  field in Product entity

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/entities/Product.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/entities/Product.java
@@ -2,10 +2,14 @@ package com.prashant.apna.bazar.entities;
 
 import java.util.List;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,9 +32,20 @@ public class Product {
   private double discount;
   private double finalPrice;
   private boolean stock;
-  private int stockQuantity;
+  private Long stockQuantity;
+
+  @Column(columnDefinition = "TEXT")
   private String description;
+
+  @ElementCollection
+  @CollectionTable(name = "product_public_ids", joinColumns = @JoinColumn(name = "product_id"))
+  @Column(name = "public_id", columnDefinition = "TEXT")
   private List<String> publicId;
+
+  @ElementCollection
+  @CollectionTable(name = "product_pics", joinColumns = @JoinColumn(name = "product_id"))
+  @Column(name = "pic_url", columnDefinition = "TEXT")
   private List<String> pic;
+
   private Boolean active;
 }

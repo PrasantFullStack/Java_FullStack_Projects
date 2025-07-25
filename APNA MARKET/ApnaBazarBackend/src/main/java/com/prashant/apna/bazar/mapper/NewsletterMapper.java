421598d8a4d49c515f315f5110a12635b7565d11
Prasant Singh <prasant2@amityonline.com>
2025-07-25 22:13:41 +0530

    commit

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/NewsletterMapper.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/mapper/NewsletterMapper.java
@@ -11,7 +11,6 @@ import com.prashant.apna.bazar.payload.response.NewsResponseDto;
 @Component
 public class NewsletterMapper {
   // map dto to entity
-
   public Newsletter toEntity(NewsletterDto newsletterDto) {
     Newsletter newsletter = new Newsletter();
     BeanUtils.copyProperties(newsletterDto, newsletter);
@@ -19,7 +18,6 @@ public class NewsletterMapper {
   }
 
   // map entity to Response
-
   public NewsResponseDto toResponse(Newsletter savedNewsletter) {
     NewsResponseDto response = new NewsResponseDto();
     BeanUtils.copyProperties(savedNewsletter, response);

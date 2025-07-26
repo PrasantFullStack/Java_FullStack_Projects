be18495a5b758a415fb3213ab494a1b8359ad26d
Prasant Singh <prasant2@amityonline.com>
2025-07-27 02:46:02 +0530

    categorySlider set item.pic

--- APNA MARKET/Frontend/ecom/src/Components/CategorySlider.jsx
+++ APNA MARKET/Frontend/ecom/src/Components/CategorySlider.jsx
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode, Pagination } from 'swiper/modules';
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
-
-import { getMaincategory } from "../Redux/ActionCreators/MaincategoryActionCreators"
-import { getSubcategory } from "../Redux/ActionCreators/SubcategoryActionCreators"
-import { getBrand } from "../Redux/ActionCreators/BrandActionCreators"
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { getMaincategory } from "../Redux/ActionCreators/MaincategoryActionCreators";
+import { getSubcategory } from "../Redux/ActionCreators/SubcategoryActionCreators";
+import { getBrand } from "../Redux/ActionCreators/BrandActionCreators";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 export default function CategorySlider({ title }) {
-  let [data, setData] = useState([])
+  let [data, setData] = useState([]);
 
-  let MaincategoryStateData = useSelector(state => state.MaincategoryStateData)
-  let SubcategoryStateData = useSelector(state => state.SubcategoryStateData)
-  let BrandStateData = useSelector(state => state.BrandStateData)
-  let dispatch = useDispatch()
+  let MaincategoryStateData = useSelector(
+    (state) => state.MaincategoryStateData
+  );
+  let SubcategoryStateData = useSelector((state) => state.SubcategoryStateData);
+  let BrandStateData = useSelector((state) => state.BrandStateData);
+  let dispatch = useDispatch();
 
-  let [showPerPage, setShowPerPage] = useState(title === "Brand" ? 6 : 3)
+  let [showPerPage, setShowPerPage] = useState(title === "Brand" ? 6 : 3);
 
   let options = {
     slidesPerView: showPerPage,
@@ -31,43 +32,43 @@ export default function CategorySlider({ title }) {
       clickable: true,
     },
     modules: [FreeMode, Pagination],
-    className: "mySwiper"
-  }
+    className: "mySwiper",
+  };
 
   function handelWindoResize() {
-    if (window.innerWidth < 576)
-      setShowPerPage(1)
-    else if (window.innerWidth < 768)
-      setShowPerPage(2)
-    else
-      setShowPerPage("Brand" ? 6 : 3)
+    if (window.innerWidth < 576) setShowPerPage(1);
+    else if (window.innerWidth < 768) setShowPerPage(2);
+    else setShowPerPage("Brand" ? 6 : 3);
   }
   window.addEventListener("resize", handelWindoResize);
 
   useEffect(() => {
-    dispatch(getMaincategory())
-  }, [MaincategoryStateData.length])
+    dispatch(getMaincategory());
+  }, [MaincategoryStateData.length]);
 
   useEffect(() => {
-    dispatch(getSubcategory())
-  }, [SubcategoryStateData.length])
+    dispatch(getSubcategory());
+  }, [SubcategoryStateData.length]);
 
   useEffect(() => {
-    dispatch(getBrand())
-  }, [BrandStateData.length])
+    dispatch(getBrand());
+  }, [BrandStateData.length]);
 
   useEffect(() => {
     if (title === "Maincategory" && MaincategoryStateData.length !== 0)
-      setData(MaincategoryStateData.filter(x => x.active))
-
+      setData(MaincategoryStateData.filter((x) => x.active));
 
     if (title === "Subcategory" && SubcategoryStateData.length !== 0)
-      setData(SubcategoryStateData.filter(x => x.active))
+      setData(SubcategoryStateData.filter((x) => x.active));
 
     if (title === "Brand" && BrandStateData.length !== 0)
-      setData(BrandStateData.filter(x => x.active))
-
-  }, [title, MaincategoryStateData.length, SubcategoryStateData.length, BrandStateData.length])
+      setData(BrandStateData.filter((x) => x.active));
+  }, [
+    title,
+    MaincategoryStateData.length,
+    SubcategoryStateData.length,
+    BrandStateData.length,
+  ]);
   return (
     <section id="clients" className="clients section">
       <div className="container section-title" data-aos="fade-up">
@@ -75,21 +76,32 @@ export default function CategorySlider({ title }) {
       </div>
 
       <div className="container" data-aos="fade-up" data-aos-delay="100">
-
         <Swiper {...options}>
-          {
-            data.map((item) => {
-              return <SwiperSlide key={item.id}>
-                <Link to={`/shop?${title === "Maincategory" ? "mc" : (title === "Subcategory" ? "sc" : "br")}=${item.name}`}>
-                  <img src={`${process.env.REACT_APP_BACKEND_SERVER}${item.pic}`} style={{ height: title === "Brand" ? 100 : 300 }} className="w-100" alt="" />
+          {data.map((item) => {
+            return (
+              <SwiperSlide key={item.id}>
+                <Link
+                  to={`/shop?${
+                    title === "Maincategory"
+                      ? "mc"
+                      : title === "Subcategory"
+                      ? "sc"
+                      : "br"
+                  }=${item.name}`}
+                >
+                  <img
+                    src={item.pic}
+                    style={{ height: title === "Brand" ? 100 : 300 }}
+                    className="w-100"
+                    alt=""
+                  />
                 </Link>
               </SwiperSlide>
-            })
-          }
+            );
+          })}
           <div className="swiper-pagination"></div>
         </Swiper>
       </div>
-
-    </section >
-  )
+    </section>
+  );
 }

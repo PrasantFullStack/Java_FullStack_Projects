fba5441734d292f10a419e33784f882dd000b4a4
Prasant Singh <prasant2@amityonline.com>
2025-07-30 09:17:46 +0530

    code commit

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/payload/response/CloudinaryResponseDto.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/payload/response/CloudinaryResponseDto.java
@@ -2,6 +2,8 @@ package com.prashant.apna.bazar.payload.response;
 
 import java.util.List;
 
+import com.prashant.apna.bazar.payload.request.CloudinaryImageDto;
+
 public class CloudinaryResponseDto {
   private String message;
   private List<CloudinaryImageDto> images;

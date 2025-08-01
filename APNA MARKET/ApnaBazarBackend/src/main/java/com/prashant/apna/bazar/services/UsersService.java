902b32c6311cacf957b271830195e902d5178aba
Prasant Singh <prasant2@amityonline.com>
2025-08-01 23:06:56 +0530

    userService

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/UsersService.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/services/UsersService.java
@@ -1,10 +1,15 @@
 package com.prashant.apna.bazar.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.prashant.apna.bazar.repositories.UserRepo;
 
 @Service
 public class UsersService {
 
+  @Autowired
+  private UserRepo userRepo;
   // create a method to handle user-related operations
 
 }

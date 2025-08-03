930bafb65593925d0c1770f4fa22b9cbcca35870
Prasant Singh <prasant2@amityonline.com>
2025-08-03 23:22:25 +0530

    Init comit In UserController createUser

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/UserController.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/UserController.java
@@ -1,10 +1,19 @@
 package com.prashant.apna.bazar.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prashant.apna.bazar.payload.request.UserDto;
+import com.prashant.apna.bazar.payload.response.UserResponse;
 import com.prashant.apna.bazar.services.UsersService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -12,5 +21,11 @@ public class UserController {
 
   @Autowired
   private UsersService usersService;
+
+  // define create user endpoint
+  @PostMapping
+  ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserDto userDto) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(usersService.createUser(userDto));
+  }
 
 }

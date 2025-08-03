d069099026c224bb24930beb5b8a4e3b0ec43d76
Prasant Singh <prasant2@amityonline.com>
2025-08-03 23:13:15 +0530

    commit init

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/UserController.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.prashant.apna.bazar.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.prashant.apna.bazar.services.UsersService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+  @Autowired
+  private UsersService usersService;
 
 }

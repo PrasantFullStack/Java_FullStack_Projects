c2e9fa59612eddd528fa917c83e6eac11140811e
Prasant Singh <prasant2@amityonline.com>
2025-08-10 23:03:42 +0530

    init commit

--- Java Full Stack 2025/Core Java/string/RepeatStringProgram.java
+++ Java Full Stack 2025/Core Java/string/RepeatStringProgram.java
@@ -2,4 +2,18 @@ package string;
 
 public class RepeatStringProgram {
 
+  // Java program to find the longest without
+  // repeating characters
+  public static void main(String[] args) {
+    String str = "Hello Prashant's World";
+    StringBuilder repeatedChars = new StringBuilder();
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      // Append the character twice
+      repeatedChars.append(ch).append(ch);
+    }
+
+    System.out.println("String with each character repeated: " + repeatedChars.toString());
+  }
 }

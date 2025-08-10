b3e4977e11831568c060558456a082c92aeb4f83
Prasant Singh <prasant2@amityonline.com>
2025-08-10 23:10:33 +0530

    Find first and last element of Arraylist

--- Java Full Stack 2025/Core Java/arrays/FindFristAndLastArraysList.java
+++ Java Full Stack 2025/Core Java/arrays/FindFristAndLastArraysList.java
@@ -1,5 +1,26 @@
 package arrays;
 
-public class FindFristAndLastArraysList {
+import java.util.ArrayList;
 
+public class FindFristAndLastArraysList {
+  // Find first and last element of
+  // Arraylist
+
+  public static void main(String[] args) {
+    ArrayList<String> list = new java.util.ArrayList<>();
+    list.add("Apple");
+    list.add("Banana");
+    list.add("Cherry");
+    list.add("Date");
+
+    if (!list.isEmpty()) {
+      String firstElement = list.get(0);
+      String lastElement = list.get(list.size() - 1);
+
+      System.out.println("First element: " + firstElement);
+      System.out.println("Last element: " + lastElement);
+    } else {
+      System.out.println("The list is empty.");
+    }
+  }
 }

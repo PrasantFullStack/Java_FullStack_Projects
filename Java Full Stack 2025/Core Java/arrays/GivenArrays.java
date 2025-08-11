07910497ed3978926bb0fb520b00f1a6fb0eeb93
Prasant Singh <prasant2@amityonline.com>
2025-08-11 22:44:35 +0530

    commit init

--- Java Full Stack 2025/Core Java/arrays/GivenArrays.java
+++ Java Full Stack 2025/Core Java/arrays/GivenArrays.java
@@ -5,4 +5,37 @@ public class GivenArrays {
   // given [ 1,1,2,2,3,4,5,5,6,6],
   // Output – [3,4]
 
+  public static void main(String[] args) {
+    int[] arr = { 1, 1, 2, 2, 3, 4, 5, 5, 6, 6 };
+    int[] result = findUniqueElements(arr);
+
+    System.out.print("Unique elements: ");
+    for (int num : result) {
+      System.out.print(num + " ");
+    }
+  }
+
+  public static int[] findUniqueElements(int[] arr) {
+    boolean[] seen = new boolean[100]; // Assuming numbers are in range 0-99
+    int uniqueCount = 0;
+
+    for (int num : arr) {
+      if (!seen[num]) {
+        seen[num] = true;
+        uniqueCount++;
+      }
+    }
+
+    int[] uniqueElements = new int[uniqueCount];
+    int index = 0;
+
+    for (int i = 0; i < seen.length; i++) {
+      if (seen[i]) {
+        uniqueElements[index++] = i;
+      }
+    }
+
+    return uniqueElements;
+  }
+
 }

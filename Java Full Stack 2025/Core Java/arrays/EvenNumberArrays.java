547fd3d1a73e8700fce13c96e69fb6e28f182d8e
Prasant Singh <prasant2@amityonline.com>
2025-08-11 06:09:06 +0530

    commit init

--- Java Full Stack 2025/Core Java/arrays/EvenNumberArrays.java
+++ Java Full Stack 2025/Core Java/arrays/EvenNumberArrays.java
@@ -4,4 +4,21 @@ public class EvenNumberArrays {
   // 11 Java program to count Odd and
   // Even number from given array
   // Input: {1,2,3,4,5,6,7,8,9}
+
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int evenCount = 0;
+    int oddCount = 0;
+
+    for (int num : arr) {
+      if (num % 2 == 0) {
+        evenCount++;
+      } else {
+        oddCount++;
+      }
+    }
+
+    System.out.println("Even count: " + evenCount);
+    System.out.println("Odd count: " + oddCount);
+  }
 }

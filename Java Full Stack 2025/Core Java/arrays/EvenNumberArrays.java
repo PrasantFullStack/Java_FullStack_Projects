2aba7deef84fa8425ac79c7614a5c81f9e0acdc5
Prasant Singh <prasant2@amityonline.com>
2025-08-12 23:31:40 +0530

    commit init

--- Java Full Stack 2025/Core Java/arrays/EvenNumberArrays.java
+++ Java Full Stack 2025/Core Java/arrays/EvenNumberArrays.java
@@ -24,4 +24,16 @@ public class EvenNumberArrays {
 
   // explanation: This code counts the number of even and odd integers in a given
   // array.
+  // It iterates through the array, checking each number to see if it is even or
+  // odd, and increments the respective counters.
+  // Finally, it prints the counts of even and odd numbers to the console.
+  // The input array in this example is {1, 2, 3, 4, 5, 6, 7, 8, 9}.
+  // The output will be:
+  // Even count: 4
+  // Odd count: 5
+  // This code can be used to analyze the distribution of even and odd numbers
+  // in any integer array.
+  // It is a simple and efficient way to categorize numbers based on their parity.
+  // The program can be easily modified to work with different input arrays by
+  // changing the values in the `arr` variable.
 }

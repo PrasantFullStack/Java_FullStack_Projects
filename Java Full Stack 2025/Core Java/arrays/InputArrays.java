0c9a5a74e22a9ca36e01ad307c925fb09ade1543
Prasant Singh <prasant2@amityonline.com>
2025-08-14 22:52:12 +0530

    Explain this code

--- Java Full Stack 2025/Core Java/arrays/InputArrays.java
+++ Java Full Stack 2025/Core Java/arrays/InputArrays.java
@@ -32,3 +32,12 @@ public class InputArrays {
     return nonRepeatedElements;
   }
 }
+// explanation: This program identifies non-repeated elements in an array.
+// It uses a HashMap to count occurrences of each element.
+// After counting, it checks which elements have a count of 1 and adds them to a
+// list of non-repeated elements.
+// For the input array [1, 1, 2, 2, 3, 4, 5, 5, 6, 6], the output will be [3,
+// 4].
+// This code can be used to find unique elements in any integer array input.
+// It is useful in scenarios where you need to filter out duplicates and find
+// elements that appear only once in the dataset.

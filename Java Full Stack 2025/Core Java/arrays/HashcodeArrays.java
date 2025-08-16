258001de5cc14caa0ef016c86336030d535dbd8c
Prasant Singh <prasant2@amityonline.com>
2025-08-16 23:51:28 +0530

    commit init

--- Java Full Stack 2025/Core Java/arrays/HashcodeArrays.java
+++ Java Full Stack 2025/Core Java/arrays/HashcodeArrays.java
@@ -22,4 +22,16 @@ public class HashcodeArrays {
     System.out.println("Are arr1 and arr2 equal? " + areEqual);
   }
 
+  // Explanation: This program demonstrates how to use the Objects class to
+  // compute hashcodes for arrays and check their equality.
+  // It uses Objects.hash to get the hashcode of each array and
+  // Objects.deepEquals to compare the arrays for equality.
+  // For the input arrays {1, 2, 3} and {1, 2, 3}, the output will show that both
+  // arrays have the same hashcode and are equal.
+  // This code can be useful when working with collections of arrays or when
+  // implementing custom data structures that require hashcodes and equality
+  // checks.
+  // It simplifies the process of comparing arrays and ensures that the hashcode
+  // generation is consistent with the contents of the arrays.
+
 }

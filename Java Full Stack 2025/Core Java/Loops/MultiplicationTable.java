4d92e545faf06bc4301609fd8219d39f2f61e904
Prasant Singh <prasant2@amityonline.com>
2025-08-18 23:44:56 +0530

    commit init

--- Java Full Stack 2025/Core Java/Loops/MultiplicationTable.java
+++ Java Full Stack 2025/Core Java/Loops/MultiplicationTable.java
@@ -12,4 +12,14 @@ public class MultiplicationTable {
             System.out.println(num + " x " + i + " = " + (num * i));
         }
     }
+    // explanation: This program prints the multiplication table of a given number.
+    // It prompts the user to enter a number and then uses a for loop to iterate
+    // from 1 to 10, multiplying the input number by the loop index (i) to generate
+    // the multiplication results.
+    // For example, if the user inputs 5, the output will be:
+    // 5 x 1 = 5
+    // 5 x 2 = 10
+    // 5 x 3 = 15
+    // ...
+    // This code can be used to generate multiplication tables for any integer input
 }

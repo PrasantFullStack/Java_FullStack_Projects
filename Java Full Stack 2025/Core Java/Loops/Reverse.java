ebc9a876a261651fdecbcc8be5d479931e585b4a
Prasant Singh <prasant2@amityonline.com>
2025-08-19 23:51:02 +0530

    commit init

--- Java Full Stack 2025/Core Java/Loops/Reverse.java
+++ Java Full Stack 2025/Core Java/Loops/Reverse.java
@@ -10,4 +10,13 @@ public class Reverse {
     }
     System.out.println("Reversed Number: " + rev);
   }
+
+  // explanation: This program reverses a given integer number.
+  // It uses a while loop to extract each digit from the number by taking the
+  // remainder when divided by 10, and constructs the reversed number by
+  // multiplying the current reversed number by 10 and adding the extracted digit.
+  // The process continues until the original number becomes 0.
+  // For example, if the input is 12345, the output will be "Reversed Number:
+  // 54321".
+  // This code can be used to reverse any integer input by the user.
 }

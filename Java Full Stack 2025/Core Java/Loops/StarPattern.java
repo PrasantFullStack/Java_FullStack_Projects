77d5c8ab30c898f58cbf2a6a4f50e1971e26896a
Prasant Singh <prasant2@amityonline.com>
2025-08-19 23:51:57 +0530

    print * prattern

--- Java Full Stack 2025/Core Java/Loops/StarPattern.java
+++ Java Full Stack 2025/Core Java/Loops/StarPattern.java
@@ -11,5 +11,19 @@ public class StarPattern {
       System.out.println();
     }
   }
+  // explanation: This program prints a right-angled triangle pattern using
+  // asterisks (*).
+  // It uses nested loops where the outer loop controls the number of rows and the
+  // inner loop
+  // controls the number of asterisks printed in each row.
+  // For example, if rows = 5, the output will be:
+  // *
+  // * *
+  // * * *
+  // * * * *
+  // * * * *
+  // This code can be modified to change the number of rows or the character used
+  // for the
+  // pattern.
 
 }

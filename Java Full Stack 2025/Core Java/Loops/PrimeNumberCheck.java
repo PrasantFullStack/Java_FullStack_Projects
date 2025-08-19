b3e9a5ae6eb7ae9003dfed65fe5ac513676a5319
Prasant Singh <prasant2@amityonline.com>
2025-08-19 23:50:17 +0530

    commit init

--- Java Full Stack 2025/Core Java/Loops/PrimeNumberCheck.java
+++ Java Full Stack 2025/Core Java/Loops/PrimeNumberCheck.java
@@ -21,4 +21,18 @@ public class PrimeNumberCheck {
 
     }
   }
+  // explanation: This program checks if a given number is prime.
+  // A prime number is a natural number greater than 1 that cannot be formed by
+  // multiplying two smaller natural numbers.
+  // The program initializes a number and checks for factors from 2 up to half of
+  // the
+  // number. If any factor divides the number evenly, it is not prime; otherwise,
+  // it is prime.
+  // The result is printed to the console, indicating whether the input number is
+  // prime or not
+
+  // For example, if the input is 23, the output will be "This number is Prime".
+  // This code can be used to check the primality of any integer and can be easily
+  // modified
+  // to handle larger numbers or different input types.
 }

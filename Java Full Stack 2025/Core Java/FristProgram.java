bcec3c5566b5921a0b6cdfb9ee1bd9811c2cab20
Prasant Singh <prasant2@amityonline.com>
2025-08-21 22:31:14 +0530

    commit init

--- Java Full Stack 2025/Core Java/FristProgram.java
+++ Java Full Stack 2025/Core Java/FristProgram.java
@@ -7,3 +7,8 @@ public class FristProgram {
 // explanation: This is a simple Java program that prints "Hello, World!" to the
 // console.
 // It serves as a basic introduction to Java syntax and structure.
+
+// The main method is the entry point of the program, and the System.out.println
+// statement is used to output text to the console.
+// This code can be modified to print different messages or perform other
+// operations.

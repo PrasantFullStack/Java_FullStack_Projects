1a0abeb00de479a75a1e006843731d9bab33fe2e
Prasant Singh <prasant2@amityonline.com>
2025-08-22 23:09:52 +0530

    commit init

--- Java Full Stack 2025/Core Java/Loops/ForLoop.java
+++ Java Full Stack 2025/Core Java/Loops/ForLoop.java
@@ -14,5 +14,11 @@ public class ForLoop {
     }
 
   }
+  // explanation: This program demonstrates the use of a for loop in Java.
+  // It prints "Hello Prashant" followed by a number from 1 to 50.
+  // The for loop initializes a counter variable (myNumber) to 1, checks if it is
+  // less than or equal to 50, and increments it by 1 in each iteration.
+  // The loop continues until the condition is no longer met.
+  // This code can be modified to change the range or the message being printed.
 
 }

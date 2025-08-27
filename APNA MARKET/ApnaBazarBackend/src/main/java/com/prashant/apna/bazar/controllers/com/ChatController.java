5241294f0f3608490c8c9cfd860b7858fffa0180
Prasant Singh <prasant2@amityonline.com>
2025-08-27 09:15:09 +0530

    commit init

--- APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/com/ChatController.java
+++ APNA MARKET/ApnaBazarBackend/src/main/java/com/prashant/apna/bazar/controllers/com/ChatController.java
@@ -3,7 +3,9 @@ package com.prashant.apna.bazar.controllers.com;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ChatController {
 
   private final ChatClient chatClient;

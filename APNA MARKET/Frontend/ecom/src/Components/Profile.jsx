7b6d5a2c16afc67c9af821c710e8fc138a89502d
Prasant Singh <prasant2@amityonline.com>
2025-09-05 23:39:55 +0530

    commit init

--- APNA MARKET/Frontend/ecom/src/Components/Profile.jsx
+++ APNA MARKET/Frontend/ecom/src/Components/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+//profile component
 export default function Profile({ title }) {
   let [data, setData] = useState({});
 

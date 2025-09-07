1162e86a9d9aabb202d877685c888e8cad201f8c
Prasant Singh <prasant2@amityonline.com>
2025-09-07 23:30:53 +0530

    Add comment to clarify Provider usage in index.js

--- APNA MARKET/Frontend/ecom/src/index.js
+++ APNA MARKET/Frontend/ecom/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 
 import Store from "./Redux/Store"
 const root = ReactDOM.createRoot(document.getElementById("root"))
-
+// Wrap the App component with the Provider and pass the store as a prop
 root.render(
     <>
         <Provider store={Store}>

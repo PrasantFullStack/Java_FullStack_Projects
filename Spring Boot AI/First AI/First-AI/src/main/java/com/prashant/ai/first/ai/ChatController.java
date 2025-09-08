a0ef6dad4d893ae9c1790642e3e91ee22ae8424a
Prasant Singh <prasant2@amityonline.com>
2025-09-08 23:47:40 +0530

    Refactor chat method in ChatController for improved functionality and clarity

--- Spring Boot AI/First AI/First-AI/src/main/java/com/prashant/ai/first/ai/ChatController.java
+++ Spring Boot AI/First AI/First-AI/src/main/java/com/prashant/ai/first/ai/ChatController.java
@@ -3,8 +3,12 @@ package com.prashant.ai.first.ai;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.experimental.var;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/chat")
@@ -15,14 +19,11 @@ public class ChatController {
   public ChatController(ChatClient.Builder builder) {
     this.chatClient = builder.build();
   }
-
   @GetMapping
-  <ResponseEntity> String chat(@RequestParam String message) {
-    return this.chatClient.chat()
-        .messages().user(message)
-        .retrieve()
-        .block()
-        .choices().get(0).message().content();
+  public ResponseEntity<String> chat(@RequestParam(value = "query", required = true) String query) {
+    String resultResponse = chatClient.prompt(query).call().content();
+    return ResponseEntity.ok(resultResponse);
+  }
   }
 
 }

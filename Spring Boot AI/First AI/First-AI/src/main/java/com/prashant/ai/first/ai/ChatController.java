519a303c1fe41dbcdf62cbdecd64ebdbd0ee45c5
Prasant Singh <prasant2@amityonline.com>
2025-09-08 23:35:45 +0530

    commit init

--- Spring Boot AI/First AI/First-AI/src/main/java/com/prashant/ai/first/ai/ChatController.java
+++ Spring Boot AI/First AI/First-AI/src/main/java/com/prashant/ai/first/ai/ChatController.java
@@ -1,8 +1,6 @@
 package com.prashant.ai.first.ai;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 

891237a438499019ab3aa34d3338279c1be272a1
Prasant Singh <prasant2@amityonline.com>
2025-09-11 23:36:02 +0530

    commit init new  AI project

---
+++ Spring Boot AI/firstai/firstai/src/main/java/com/first/ai/project/firstai/FirstaiApplication.java
@@ -0,0 +1,13 @@
+package com.first.ai.project.firstai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstaiApplication.class, args);
+	}
+
+}

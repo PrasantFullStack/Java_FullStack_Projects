1d478548fe8fe7e0b17ea47932b07bbcf0b17e2e
Prasant Singh <prasant2@amityonline.com>
2025-09-13 23:30:59 +0530

    Add launch configuration for FirstaiApplication

--- Spring Boot AI/firstai/firstai/src/main/java/com/first/ai/project/firstai/FirstaiApplication.java
+++ Spring Boot AI/firstai/firstai/src/main/java/com/first/ai/project/firstai/FirstaiApplication.java
@@ -3,6 +3,7 @@ package com.first.ai.project.firstai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// this is my first spring boot project
 @SpringBootApplication
 public class FirstaiApplication {
 

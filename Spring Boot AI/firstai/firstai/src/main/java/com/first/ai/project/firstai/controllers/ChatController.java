5a6004e2a6b127e02a3e0fae72be95453507dae3
Prasant Singh <prasant2@amityonline.com>
2025-09-15 10:26:48 +0530

    commit init

--- Spring Boot AI/firstai/firstai/src/main/java/com/first/ai/project/firstai/controllers/ChatController.java
+++ Spring Boot AI/firstai/firstai/src/main/java/com/first/ai/project/firstai/controllers/ChatController.java
@@ -1,4 +1,11 @@
-package main.java.com.first.ai.project.firstai.controllers;
+package com.first.ai.project.firstai.controllers;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 //controller class
 @RestController
@@ -11,7 +18,7 @@ public class ChatController {
     this.chatClient = builder.build();
   }
 
-  // chat method
+  // chat method to handle GET requests
   @GetMapping
   public ResponseEntity<String> chat(@RequestParam(value = "query", required = true) String query) {
     String resultResponse = chatClient

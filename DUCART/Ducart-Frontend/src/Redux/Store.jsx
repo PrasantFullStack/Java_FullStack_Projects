853de8d0972034cd8f2d1c7df2c17f28da489b5d
Prasant Singh <prasant2@amityonline.com>
2025-09-20 23:37:03 +0530

    Refactor Store.jsx for consistency and clarity by adding missing semicolons and comments

--- DUCART/Ducart-Frontend/src/Redux/Store.jsx
+++ DUCART/Ducart-Frontend/src/Redux/Store.jsx
@@ -2,15 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 
 import RootReducer from "./Reducers/RootReducer";
-import RootSaga from "./Sagas/RootSaga"
+import RootSaga from "./Sagas/RootSaga";
 
-const saga = createSagaMiddleware()
+//create the saga middleware
+const saga = createSagaMiddleware();
 
-const Store  = configureStore({
-    reducer:RootReducer,
-    middleware : ()=>[saga]
-})
+const Store = configureStore({
+  reducer: RootReducer,
+  middleware: () => [saga],
+});
 
-export default Store
+export default Store;
 
-saga.run(RootSaga)
+saga.run(RootSaga);

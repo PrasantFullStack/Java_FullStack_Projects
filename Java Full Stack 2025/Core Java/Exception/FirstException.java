7a5b4c9f946af1e219ffabe824bc80a9cf380d99
Prasant Singh <prasant2@amityonline.com>
2025-09-23 08:22:42 +0530

    exception commit init

---
+++ Java Full Stack 2025/Core Java/Exception/FirstException.java
@@ -0,0 +1,15 @@
+package Exception;
+//what is exception in java?
+
+//An exception is an event that occurs during the execution of a program that disrupts the normal flow of instructions.
+
+public class FirstException {
+  public static void main(String[] args) {
+    try {
+      int data = 100 / 1; // This will cause ArithmeticException
+    } catch (ArithmeticException e) {
+      System.out.println("Exception caught: Division by zero is not allowed.");
+    }
+    System.out.println("Rest of the code...");
+  }
+}

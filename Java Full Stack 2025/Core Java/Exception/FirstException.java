8b96c0fe45fec57316adfa07c66352ccefbdce2d
Prasant Singh <prasant2@amityonline.com>
2025-09-23 08:25:19 +0530

    commit init

--- Java Full Stack 2025/Core Java/Exception/FirstException.java
+++ Java Full Stack 2025/Core Java/Exception/FirstException.java
@@ -3,6 +3,8 @@ package Exception;
 
 //An exception is an event that occurs during the execution of a program that disrupts the normal flow of instructions.
 
+//It is an object that represents an error or an unexpected condition that arises during the runtime of a program.
+
 public class FirstException {
   public static void main(String[] args) {
     try {

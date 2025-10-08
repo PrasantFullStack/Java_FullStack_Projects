b824eb418f62b39c3d06f9c593841ffcf3313f70
Prasant Singh <prasant2@amityonline.com>
2025-10-08 23:08:47 +0530

    commit init

--- Java Full Stack 2025/Core Java/oops/basic.java
+++ Java Full Stack 2025/Core Java/oops/basic.java
@@ -6,5 +6,13 @@ package oops;
 //class is a blueprint of objects
 //object is an instance of class
 public class basic {
+  int x = 10; // non static variable
+  static int y = 20; // static variable
+
+  public static void main(String[] args) {
+    basic obj = new basic(); // creating object of class
+    System.out.println("Non static variable: " + obj.x);
+    System.out.println("Static variable: " + y);
+  }
 
 }

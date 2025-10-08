33768556cca7eb26d14eab191fc331486dfc358d
Prasant Singh <prasant2@amityonline.com>
2025-10-08 23:07:38 +0530

    what is oops?

--- Java Full Stack 2025/Core Java/oops/basic.java
+++ Java Full Stack 2025/Core Java/oops/basic.java
@@ -1,5 +1,10 @@
 package oops;
 
+//what is oops?
+//object oriented programming system 
+//class and objects
+//class is a blueprint of objects
+//object is an instance of class
 public class basic {
 
 }

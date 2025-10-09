9eb88a708da5b92d284c5aa5a6e5374c1d2c3796
Prasant Singh <prasant2@amityonline.com>
2025-10-09 23:27:05 +0530

    what is oops?

---
+++ Java Full Stack 2025/Core Java/oops/Abstraction.java
@@ -0,0 +1,7 @@
+package oops;
+
+public class Abstraction {
+  // Abstraction is the process of hiding the implementation details and showing
+  // only the functionality to the user.
+  // Abstraction is achieved using abstract classes and interfaces.
+}

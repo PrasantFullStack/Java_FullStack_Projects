99396d9a0b851cb139940e04b56171873f335bc9
Prasant Singh <prasant2@amityonline.com>
2025-10-10 23:15:36 +0530

    commit init

--- Java Full Stack 2025/Core Java/oops/Abstraction.java
+++ Java Full Stack 2025/Core Java/oops/Abstraction.java
@@ -4,4 +4,13 @@ public class Abstraction {
   // Abstraction is the process of hiding the implementation details and showing
   // only the functionality to the user.
   // Abstraction is achieved using abstract classes and interfaces.
+  // Abstraction is used to reduce complexity and increase efficiency.
+  // Abstraction is used to hide the irrelevant details and show only the
+  // relevant details.
+  // Abstraction is used to focus on what the object does instead of how it does
+  // it.
+  public static void main(String[] args) {
+    System.out.println("Abstraction in Java");
+  }
+
 }

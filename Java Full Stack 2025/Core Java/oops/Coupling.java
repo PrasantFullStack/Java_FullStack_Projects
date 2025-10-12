8620425f7aadd40e5152c5925cae77bcd3588bbc
Prasant Singh <prasant2@amityonline.com>
2025-10-12 23:17:34 +0530

    commit init

--- Java Full Stack 2025/Core Java/oops/Coupling.java
+++ Java Full Stack 2025/Core Java/oops/Coupling.java
@@ -1,5 +1,12 @@
 package oops;
 
 public class Coupling {
+  // Coupling refers to the degree of interdependence between different classes or
+  // modules in a software system.
+  // It indicates how closely connected or related different classes or modules
+  // are.
+  // There are two types of coupling:
+  // 1. Tight Coupling: In tight coupling, classes or modules are highly dependent
+  // on each other. Changes in one class may require
 
 }

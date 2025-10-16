55a1ed4b19ade16ff847f18a8a0bfa35fdefbfe9
Prasant Singh <prasant2@amityonline.com>
2025-10-16 21:05:47 +0530

    commit init

--- Java Full Stack 2025/Core Java/Exception/ExceptiomsExample.java
+++ Java Full Stack 2025/Core Java/Exception/ExceptiomsExample.java
@@ -11,6 +11,11 @@ public class ExceptiomsExample {
     System.out.println(str.length()); // This will cause NullPointerException
     int[] arr = { 1, 2, 3 };
     System.out.println(arr[5]); // This will cause ArrayIndexOutOfBoundsException
+    String s = "abc";
+    int num = Integer.parseInt(s); // This will cause NumberFormatException
+    System.out.println("Number: " + num);
+    Object x = new Integer(0);
+    System.out.println((String) x); // This will cause ClassCastException
   }
 
 }

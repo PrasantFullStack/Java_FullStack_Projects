39fd5839d22313cac334fc3bb48c40a4968d244a
Prasant Singh <prasant2@amityonline.com>
2025-11-22 22:41:09 +0530

    array declaration

--- Java Full Stack 2025/Core Java/arrays/ArrayClass.java
+++ Java Full Stack 2025/Core Java/arrays/ArrayClass.java
@@ -7,5 +7,10 @@ public class ArrayClass {
     arr[0] = 10;
     arr[1] = 20;
 
+    System.out.println("Array elements:");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+
   }
 }

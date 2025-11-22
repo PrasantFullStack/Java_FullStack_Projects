781b716669675a1756a657afb853268b69fa08a1
Prasant Singh <prasant2@amityonline.com>
2025-11-22 22:40:14 +0530

    array declaration

--- Java Full Stack 2025/Core Java/arrays/ArrayClass.java
+++ Java Full Stack 2025/Core Java/arrays/ArrayClass.java
@@ -2,6 +2,7 @@ package arrays;
 
 public class ArrayClass {
   public static void main(String[] args) {
+    // Example of array declaration and initialization in Java
     int[] arr = new int[5]; // size 5 array
     arr[0] = 10;
     arr[1] = 20;

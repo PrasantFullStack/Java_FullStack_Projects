ebba500f7384df0f284dc05ef4f263017be67ee6
Prasant Singh <prasant2@amityonline.com>
2026-01-31 10:16:21 +0530

    commit init

--- React Native Project/my-app/app/_layout.tsx
+++ React Native Project/my-app/app/_layout.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { Stack } from "expo-router";
-import { StatusBar } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar style="dark" />
 
       <Stack
+        initialRouteName="splash"
         screenOptions={{
           headerShown: false,
         }}
       >
         {/* Auth */}
+        <Stack.Screen name="splash" />
         <Stack.Screen name="login" />
         <Stack.Screen name="register" />
         <Stack.Screen name="otp" />
 
         {/* Buyer */}
-        <Stack.Screen name="(screen)/home" />
+        <Stack.Screen name="(buyer)/home" />
         <Stack.Screen name="(buyer)/product" />
         <Stack.Screen name="(user)/cart" />
         <Stack.Screen name="(user)/checkout" />
@@ -26,10 +28,10 @@ export default function RootLayout() {
         <Stack.Screen name="(user)/address" />
 
         {/* Seller */}
-        <Stack.Screen name="/dashboard" />
+        <Stack.Screen name="dashboard" />
 
         {/* Admin */}
-        <Stack.Screen name="vender/dashboard" />
+        <Stack.Screen name="vendor/dashboard" />
       </Stack>
     </>
   );

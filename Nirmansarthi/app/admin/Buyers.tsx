07f2d67df476fd69844a5d8688c0c50fb71491a6
Prasant Singh <prasant2@amityonline.com>
2026-04-23 00:15:04 +0530

    commit  init

--- Nirmansarthi/app/admin/Buyers.tsx
+++ Nirmansarthi/app/admin/Buyers.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 //buyer page
+
 export default function Buyers() {
   return (
     <View style={styles.container}>
